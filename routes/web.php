924fcc2b7df5fec73e645c3ee4fbc482e8b9d6fc
genz4341 <genz.ortiz@gmail.com>
2025-01-08 15:32:31 +0400

    First Template Appearance

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TemplateController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [TemplateController::class, 'index']);
